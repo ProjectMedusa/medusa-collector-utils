b8950e7536d6c8eabeb22c5d616249ff15a4c1aa
AidasPa <aidas0330@gmail.com>
2020-08-31 22:19:43 +0300

    1.1.3

--- src/Collector.js
+++ src/Collector.js
@@ -14,17 +14,44 @@ class Collector {
   async parseTableWithParser(html) {
     const { document } = new JSDOM(html).window;
     const titles = document.querySelectorAll('.Title');
+
     titles.forEach((title) => {
       const parent = title.parentElement;
-      if (title.innerHTML.includes('DECLARED')) {
-        const rows = parent.querySelectorAll('table > tbody > tr');
+      // if (title.innerHTML.includes('DECLARED') && !title.innerHTML.includes('REDUCED')) {
+      if (/declared/i.test(title.innerHTML) && !(/reduced/i).test(title.innerHTML)) {
+        const table = parent.querySelectorAll('table')[0];
+        const rows = table.querySelectorAll('tbody > tr');
         this.parser.runwayRows(rows);
       }
     });
+    // check for intersection table index
+    if (this.parser.getIntersectionTable()) {
+      titles.forEach((title) => {
+        const parent = title.parentElement;
+        // if (title.innerHTML.includes('DECLARED') && !title.innerHTML.includes('REDUCED')) {
+        if (/declared/i.test(title.innerHTML) && !(/reduced/i).test(title.innerHTML)) {
+          const table = parent.querySelectorAll('table')[this.parser.getIntersectionTable()];
+          const rows = table?.querySelectorAll('tbody > tr');
+          this.parser.intxRows(rows);
+        }
+      });
+    }
+    // check for intersection table name
+    if (this.parser.getIntersectionTableTitle()) {
+      // name set, look for another table
+      titles.forEach((title) => {
+        const parent = title.parentElement;
+        if (title.innerHTML.includes(this.parser.getIntersectionTableTitle())) {
+          const rows = parent.querySelectorAll('table > tbody > tr');
+          this.parser.intxRows(rows);
+        }
+      });
+    }
+
     // run loop again
     titles.forEach((title) => {
       const parent = title.parentElement;
-      if (title.innerHTML.includes('PHYSICAL')) {
+      if (/physical/i.test(title.innerHTML)) {
         const table = parent.querySelectorAll('table')[this.parser.getRunwayCharasteristicsTable()];
         const rows = table.querySelectorAll('tbody > tr');
         this.parser.runwayCharacteristics(rows);
@@ -34,12 +61,12 @@ class Collector {
 
   async retrieveTable() {
     await page().goto(this.parser.getLink());
-    await page().waitForSelector('frame[name=eAISContent]');
+    // await page().waitForSelector('frame[name=eAISContent]');
 
-    const frame = page().frames().find((item) => item.name() === 'eAISContent');
-    await frame.waitForNavigation();
+    // const frame = page().frames().find((item) => item.name() === 'eAISContent');
+    // await frame.waitForNavigation();
     // eslint-disable-next-line no-undef
-    return frame.evaluate(() => document.querySelector('body').innerHTML);
+    return page().evaluate(() => document.querySelector('body').innerHTML);
   }
 }
 
--- src/Parser.js
+++ src/Parser.js
@@ -2,11 +2,16 @@
 const fs = require('fs').promises;
 
 module.exports = class Parser {
-  constructor(airport, link, runwayCharacteristicsTable = 0) {
+  constructor(airport,
+    link, runwayCharacteristicsTable = 0,
+    intersectionTableTitle = false,
+    intersectionTable = false) {
     this.airport = airport;
     this.results = [];
     this.runwayCharacteristicsTable = runwayCharacteristicsTable;
     this.constructSourceLink(link);
+    this.intersectionTableTitle = intersectionTableTitle;
+    this.intersectionTable = intersectionTable;
   }
 
   constructSourceLink(link) {
@@ -17,16 +22,29 @@ module.exports = class Parser {
     return this.link;
   }
 
+  getIntersectionTableTitle() {
+    return this.intersectionTableTitle;
+  }
+
+  getIntersectionTable() {
+    return this.intersectionTable;
+  }
+
   getRunwayCharasteristicsTable() {
     return this.runwayCharacteristicsTable;
   }
 
   async save() {
+    this.beforeSave();
     await fs.writeFile(
       `results/${this.airport}.json`,
       JSON.stringify(this.results, null, '\t'),
       'utf-8',
     );
+  }
+
+  beforeSave() {
+
   }
 
   runwayRows() {
@@ -35,5 +53,13 @@ module.exports = class Parser {
 
   runwayCharacteristics() {
     throw new Error('Method must be overrided!');
+  }
+
+  override(icao, callback) {
+    if (this.airport === icao) {
+      // eslint-disable-next-line no-console
+      console.log(`Overriding ${icao}...`);
+      this.results = callback(this.results);
+    }
   }
 };
